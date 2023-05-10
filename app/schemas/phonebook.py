a0b04090ef6d7019f8e188876e8a0b342075a154
Timmy-Edibo <timothyedibo@gmail.com>
2023-05-10 21:45:47 +0100

    added phone_number validation

--- app/schemas/phonebook.py
+++ app/schemas/phonebook.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 
 
 class Phonebook(BaseModel):
-    firstname: str
-    lastname: str 
-    phone_number:str = Field(max_length=10)
+    firstname: str = Field(min_length=3)
+    lastname: str = Field(min_length=3)
+    phone_number:str = Field(max_length=10, min_length=10)
+    
+    # @validator('lastname')
+    # def validate_lastname(cls, values):
+    #     ph = values.get('lastname')
+    
+    #     if not isinstance(ph, str):
+    #         raise ValueError('Value must be a string')
+    
+  
