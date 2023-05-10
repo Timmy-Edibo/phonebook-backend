7f4cf88449855f53e603551e7098f16a5cf2b63f
Timmy-Edibo <timothyedibo@gmail.com>
2023-05-10 21:53:06 +0100

    added phone_number validation

--- app/schemas/phonebook.py
+++ app/schemas/phonebook.py
@@ -3,9 +3,10 @@ from typing import Optional
 
 
 class Phonebook(BaseModel):
-    firstname: str = Field(min_length=3)
-    lastname: str = Field(min_length=3)
-    phone_number:str = Field(max_length=10, min_length=10)
+    firstname: str = Field(min_length=3, title="First Name", example="John")
+    lastname: str = Field(min_length=3, title="Last Name", example="Doe")
+    phone_number: str = Field(max_length=10, min_length=10, title="Phone Number", example="1234567890")
+    
     
     # @validator('lastname')
     # def validate_lastname(cls, values):
