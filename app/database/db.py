466dabce081dac96316c5ca0400e39f240442b9a
Timmy-Edibo <timothyedibo@gmail.com>
2023-06-07 06:26:49 +0100

    changed db url

--- app/database/db.py
+++ app/database/db.py
@@ -11,12 +11,12 @@ DATABASE_USER=os.environ.get("DATABASE_USER")
 DATABASE_PASSWORD=os.environ.get("DATABASE_PASSWORD") 
 DATABASE_HOST=os.environ.get("DATABASE_HOST")
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./phonebook.db"
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = "sqlite:///./phonebook.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:RsOUXuROlrbMlzx3aNiq@containers-us-west-101.railway.app:6064/railway"
-print(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:RsOUXuROlrbMlzx3aNiq@containers-us-west-101.railway.app:6064/railway"
+# print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
